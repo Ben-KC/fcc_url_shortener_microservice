660ef37aee879d4c8c2efe9da68e1a379f5cb26c
Benjamin Cox <benny24c@gmail.com>
2016-11-07 06:47:50 +0000

    add initial routing for /new

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
-var express = require('express');
+var express = require('express'),
+    mongo = require('mongodb').MongoClient;
 var app = express();
 
+
 app.set('port', (process.env.PORT || 8080));
+
+//base page
+app.use('/', express.static('public'));
+
+//in case they forget to include a url
+app.get('/new', function(req, res) {
+    res.send(JSON.stringify({error: "please include an address"}));
+});
+
+//set a url
+//the regex in the get method allows for the submitted url to have slashes
+app.get(/^\/new\/(.+)/, function(req, res) {
+    //test the url
+    var test = /^https?:\/\/(www\.)?\w+\.\w+$/;
+    var url = req.params[0];
+
+    if(!test.test(url)) {
+        return res.send(JSON.stringify({error: "invalid address"})); 
+    }
+
+    res.send('it worked!');
+});
 
 app.listen(app.get('port'));
